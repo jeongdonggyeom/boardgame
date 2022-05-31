6f26e3ac2cd4a325f1b12b0f89adf8774da46a4c
jeongdonggyeom <topcome928@gmail.com>
2022-05-31 21:20:02 +0900

    fix: cors 오류 해결

--- React/gangseogu/src/components/weather.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Weather from "../weather.json";
-
-export default function navbar(){
-    console.log(Weather.response.body.dataType);
-    return(
-        <h1>HI</h1>
-    )
-}
